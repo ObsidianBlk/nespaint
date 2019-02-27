ad0b29b0dcfad2bda4b0652181c95094a8e997ae
Bryan Miller <bmiller1008@gmail.com>
2019-02-26 19:15:40 -0500

    Server now uses ejs to serve the index html. Broke the original index.html file into ejs file located in the /views folder.

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require("express");
 const app = express();
 var port = 8000;
 
+app.set('views', path.join(__dirname, "/views"));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use("/app", express.static(path.join(__dirname, "/app")));
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.render('index.html');
 });
 
-app.listen(port, () => console.log("NESPaint listening on port ${port}!"));
+app.listen(port, () => console.log("NESPaint listening on port " + port + "!"));
